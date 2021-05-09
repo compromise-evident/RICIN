c23513e0e695212e87712cbbfb0e3f55ded87cc6
Nikolay Valentinovich Repnitskiy <75550631+compromise-evident@users.noreply.github.com>
2021-05-09 10:14:12 -0600

    Get started on your own

--- fabricate_custom_audio.cpp
+++ fabricate_custom_audio.cpp
@@ -18,8 +18,8 @@ int main()
 	//Writes silence.
 	for(int a = 0; a < 40000; a++) {out_stream.put(127);}
 	
-	//Number of bytes to produce (repeats this pattern n times, currently set to 220kB.)
-	for(int n = 0; n < 220000; n++)
+	//Number of bytes to produce (repeats this pattern n times, currently set to 1kB.)
+	for(int n = 0; n < 1000; n++)
 	{	out_stream.put(-118); for(int a = 0; a < 19; a++) {out_stream.put(127);} //Spike, long  wait = 0
 		out_stream.put(-118); for(int a = 0; a < 19; a++) {out_stream.put(127);} //Spike, long  wait = 0
 		out_stream.put(-115); for(int a = 0; a <  9; a++) {out_stream.put(127);} //Spike, short wait = 1
